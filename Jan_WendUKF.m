05b217d18cf2b0261af8bcad81cfce8b7c835c05
Richard Balson <richard.balson@outlook.com>
2013-08-05 10:56:12 +1000

    initial files for estimation procedure

---
+++ Jan_WendUKF.m
@@ -0,0 +1,246 @@
+function [X, Pxx, X_Multi, Pxx_Multi]= Jan_WendUKF
+% script created by Richard Balson 05/08/2013
+
+% description
+% ~~~~~~~~~~~
+% This script estimates synaptic gains of the Wendling neural mass
+% model using the unscented Kalman filter, it also simualtes the states and
+% output of the Jansen neural mass model. In this script states and
+% parameters are realisations with a set mean and a defined standard
+% deviation
+
+% last edit
+% ~~~~~~~~~
+
+% Altered standard deviation for input, altered uncertainty in input, made
+% limit on estimate of input
+
+% next edit
+% ~~~~~~~~~
+
+% Work on flipping the observations and rerunning estimation on same data
+% set multiple times using initial values that are equal to the final
+% values from the previous estimation results
+
+% Sort out the observation used for the updating of the state estimate,
+% next to use next period observation
+
+% Beginning of script
+% ~~~~~~~~~~~~~~~~~~~~~
+
+% Clear workspace
+% ~~~~~~~~~~~
+clear
+close all
+clc
+
+tic
+
+addpath(genpath(pwd)); % Specify files required for estimation
+
+system_dependent('setprecision',64); % Set the precision of accuracy in order to reduce the effect of rounding errors.
+
+User_defined_parameters;
+
+dt = 1/fs;
+
+% Dynamic variables
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+if filter_simulation
+    band_coeff = filtercoeff(lowcutoff,highcutoff,fs);
+end
+
+% Estimation Procedure Parameters
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Dx = Ds+Dp+Dk; % Number of dimensions of augmented state matrix, Note that estimated parameters and inputs are now considered to be 'slow states' in the estimation procedure
+
+% Physiological range of Model gains
+% ~~~~~~~~~~~~~~~~~
+
+Max_A =10;
+Min_A =0;
+Max_B =40;
+Min_B =0;
+Max_G =40;
+Min_G =0;
+
+Max = [Max_A, Max_B, Max_G];
+Min = [Min_A, Min_B, Min_G];
+
+frequency_limits =[30 150];
+
+for q = 1:Simulation_number
+    
+    if Random_number_generator(1)
+        rng(0);
+    else
+        rng(cputime);
+    end
+    if simulate
+        SimulationSettings.fs = fs;
+        Jansen_Simulation(SimulationSettings); % Simulate states and output of the extended neural mass model
+    end
+    load([SimulationSettings.name,'.mat']);% Load parameters from simulation
+    % output8 contains the simulated
+    % model output; normalised_gaussian
+    % input contains the input to the
+    % model; sampling_frequency
+    % contains the sampling frequency
+    % used for simulation; stochastic
+    % decides whether the variance is
+    % applied to the input; MVI
+    % contains the model gains; tcon
+    % the model time constants; C the
+    % connectivity constants and
+    % frequency_limits the limits for
+    % the input frequency
+    
+    % Simulated signal data mV
+    % ~~~~~~~~~~~~~~~~
+    output = output/Data_scale;
+    if filter_simulation
+        output = filtfilt1(band_coeff,1,output);
+    end
+    
+    % Static Variables
+    % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    % ~~~~~~~~~~~
+    EstimationVariables;
+    init =0;
+    condition =1;
+    while condition
+        init=init+1;
+        conditionT = init<Reinitialise_parameters_attempts*(~Parameter_initialisation);
+        Initialise_parameters;
+        
+        %%
+        
+        % UKF algorithm
+        % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        count(q) = init;
+        
+        for p =1:Number_of_observations
+            [Sigma(:,:,p) err] = Unscented_transform(Dx,Pxx(:,:,p),X(:,p),kappa);
+            if (err ==1)
+                break
+            end
+            %
+           
+            if Dk ==1
+                %                     if (LimitEst ==1)
+                %                         if (Sigma(Ds+1,k,p) < 0)
+                %                             Sigma(Ds+1,k,p) = 0;
+                %                         end
+                %                     end
+                Input_var = Sigma(Ds+1,:,p);
+            else
+                Input_var = Input_mean;
+            end
+            [Xout(:,:,p) Yout(:,:,p)] = WNM(Sigma(:,:,p),dt,Input_var, gain, tcon,C);
+            [ExpX(:,p) ExpY(:,p) Pxxn Pxyn Pyyn] = Expectation(Xout(:,:,p), Dx, Yout(:,:,p), 1,kappa);
+            
+            Pyyn = Pyyn +R;
+            %
+            Pxxn = Pxxn + Q;
+            
+            [X(:,p+1) Pxx(:,:,p+1)] = Kalman(ExpX(:,p), ExpY(:,p), Y(p), Pxxn, Pxyn, Pyyn);
+            if StepbyStepCheck % perform a step by step check of results
+                stepn = 1:p;
+                stepC = 0:p;
+                figure % Plot the expected value of Y and the observed Y
+                plot(stepn,Y(1:p),'k');
+                hold on
+                plot(stepn, ExpY(:,1:p));
+                hold on
+                plot(stepC,X(2,1:p+1)-X(3,1:p+1),'r')
+                PxxE(:,:,stepn(end)) = Pxxn;
+                for zz = 1:Ds
+                    figure
+                    
+                    for li = stepn
+                        ernE(li) = ExpX(zz,li) - PxxE(zz,zz,li);
+                        erpE(li) = ExpX(zz,li) + PxxE(zz,zz,li);
+                    end
+                    for li = stepC+1
+                        ern(li) = X(zz,li) - Pxx(zz,zz,li);
+                        erp(li) = X(zz,li) + Pxx(zz,zz,li);
+                    end
+                    plot(stepn, ExpX(zz,1:p),'b');
+                    hold on
+                    plot(stepn,z(EstStart_Sample:EstStart_Sample+p-1,zz),'k');
+                    hold on
+                    plot(stepC, X(zz,1:p+1),'r');
+                    hold on
+                    plot(stepC, ern(1:p+1),'g+');
+                    hold on
+                    plot(stepC,  erp(1:p+1),'g+');
+                    hold on
+                    plot(stepn,  erpE(1:p),'m+');
+                    hold on
+                    plot(stepn,  ernE(1:p),'m+');
+                end
+                pause
+                close all
+            end
+            
+        end
+        
+       conditionT1 = ((X(Ds+Dk+1,end) <0) || (X(Ds+Dk+2,end) <0) || (X(Ds+Dk+3,end) <0));
+
+        
+        condition = conditionT && conditionT1;
+    end
+    toc
+    
+    if (Simulation_number ~=1)
+        if q ==1
+            X_Multi = zeros(floor(size(X,2)/Decimate)+1,Dp+Dk,Simulation_number);
+            Pxx_Multi = zeros(floor(size(X,2)/Decimate)+1,Dp+Dk,Simulation_number);
+        end
+        X_Multi(:,:,q) = X(Ds+1:Ds+Dp+Dk,1:500:end)';
+        for k =1:Dk+Dp
+            Pxx_Multi(:,k,q) = squeeze(Pxx(k,k,1:500:end));
+        end
+    else
+        X_Multi =0;
+        Pxx_Multi =0;
+    end
+    PC = q/Simulation_number
+end % End Simulation_nuumber loop
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%Post processing and image handling%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%     % Determne how to plot estimation output
+%     % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%
+if (tstart+zoomtime) >= (SimulationSettings.simulation_time-EstStart)
+    zoomtime = SimulationSettings.simulation_time-EstStart-tstart-0.5;
+end
+
+Generate_figures;
+
+if fig_save
+    
+    Figure_handling;
+    
+end
+if Simulation_number>1
+    Generate_figures_multi;
+    if fig_save
+        
+        Figure_handling_multi;
+        
+    end
+end
+% load Handel
+% sound(y,Fs)
+%
+% toc
+%
+%
---
+++ User_defined_parameters.m
@@ -0,0 +1,178 @@
+% script created by Richard Balson 05/08/13
+% This script specifies all neccesary parameters for the estimation of the
+% Wendling model using simulation data from the Jansen and Rit model.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+% General parameters
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+filter_simulation =0; % Specify whether or not to filter simulated data
+
+Data_scale =1; % Scale simulation output to determine the effect of scaling on estimation out = outOrig/Data_scale
+
+if filter_simulation
+    highcutoff = 2.5; % Specify highcutoff frequency for filter
+    
+    lowcutoff = 40; % Specify low cutoff frequency for filter
+    % Data will have frequency content between highcutoff and lowcutoff
+end
+
+fs =2048; % Specify sampling frequency
+
+Simulation_number = 20; % Specify number of simulations to estimate
+
+if Simulation_number>1
+    Decimate = 500; % Specify the distance between corresponding samples for the output matrix when multiple simulation are performed
+else Decimate =1;
+end
+
+Random_number_generator=[0 0]; % Specify whether random numbers for simulation should be different or the same for numerous simulations;
+
+StepbyStepCheck =0; % Check how prediction and correction steps are working on a plot
+
+LimitEst = 0; % If set to 1 estimation results are limited by physiological parameters within the estimation procedure, that is sigma points cannot be aboe the physiological parameters
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+% Simulation decisions
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+simulate =1; % Decide whether to simulate model output or use previous results if simulate is equal to 1 then observations for estimatio purposes are resimulated
+
+NoiseIn = 1e-3;% Base 1e-2 Specify noise to add to simulated signal
+SimulationSettings.name = 'Jansen_output'; % Specify name of file to save Wendling model output data to, or to load data from when simulation is not performed
+if simulate % Specify parameters for simulation purposes
+    SimulationSettings.simulation_time =100; %Time for simulation in seconds 
+    SimulationSettings.slope_time =1; % Specifies the time over which the model gain should be altered
+    SimulationSettings.number_of_sigma_input = 1; % Used to determine standard deviation of input if  1: 68.27% of realisations within physiolgical range, 2: 95.45, 3: 99.73 4: 99.994
+    SimulationSettings.stochastic = 0.5; % Used to specifiy the stochastic adjustment on the input 1 is no adjustment. <1 downscalling, >1 upscaling
+    SimulationSettings.Parameter_index = 7; % Choose parameters to be simulated: 1 = Seizure Parameter from Wendling 2002;
+    %  2 = Seizure Parameter from Wendling 2005;...
+    %  3 = Altered excitability;
+    %  4 = Parameters at midpoint of their range;
+    %  5 = random parameters;
+    %  6= random parameters and random number of
+    %  variations in simulation
+    % 7 User defined For Jansen Simulation G parameter ignored
+    if SimulationSettings.Parameter_index==7 % Specify synaptic gains for simulation
+        SimulationSettings.AV= [5 7 5 5 5];
+        SimulationSettings.BV= [20 20 20 30 20];
+    end
+    SimulationSettings.Input_mean_variation = 0; % If 0 mean stays constant for simulation,
+    %if 1 input mean is drawn from a uniform distribution limited by the physiological limits of the input
+    % if 2 input mean is drawn from a Gaussian
+    % distribution with a mean as per Wendling
+    % 2002 and standard deviation that satisfies
+    % number_of_sigma_input
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+% Estimation decisions
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+MeanCheckTStart = 5; % For multiple simulations determine where to start checking the estimation values
+
+EstStart = 5; % Specify the duration after simulation start when estimation should start. This allows removal of all transients.
+
+% Estimation states
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Ds = 8; % Number of differential equations describing model, also the number of fast states to be estiamted
+
+Dp = 3; % Number of parameters to be estimated, also refered to as slow states
+
+Dk =1; %If set to 1 the mean of the stochastic input will be estimated % Note that if Input_mean_variation is not zero than Dk should be set to one to allow tracking of the input mean
+
+Dy =1; % Number of observable outputs from the simulation
+
+% Intialisation of parameters
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Parameter_initialisation = 0; % 0 for parameters to be initialised by Gauss distribution
+% 1 for parameters to be initialsed as a
+% percetage error from actual values
+
+if Parameter_initialisation
+    PercError = 10; % Specify percentage error for parameter intialisation
+end
+
+number_of_sigma = 1; % Number of standard deviations from mean. 4 accounts for 99.73 percent of points.
+
+Reinitialise_parameters_attempts = 1; % Specify number of attempts for parameter reinitialisation if results are not physiologucally possible
+
+% Uncertainty parameters
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+kappa =0; % Varibale used to define the relative contribution of the mean on the propogation of states, and adjustment of the variance of the sigma points drawn from the Gaussian distribution
+
+Variable_state_uncertainty = 0;%1e-3; % 1e-3 Uncertianty due to stochastic input
+
+State_uncertainty_adjustment = [1 2 3 40 1 2 3 40];% Exponential decrease in uncertainty % All ones good for slow but steady convergence
+
+Exc_parameter_uncertainty = 1e-2;
+SInh_parameter_uncertainty =8e-2;
+FInh_parameter_uncertainty =8e-2;
+Base_parameter_uncertainty = 8e-2;%1e-2;%1e-6;%1e-3; % Inherent parameter uncertainty due to model error
+
+Variable_parameter_uncertainty = 0;%1e-3;  % Uncertianty due parameters varying in time
+
+Base_input_uncertainty = 1e-12;%1e-3; % Inherent parameter uncertainty due to model error
+
+Variable_input_uncertainty =0;%1e-3; % Uncertianty due varying input mean, Set to zero if the input mean is not varying
+
+Observation_uncertainty = 1e-3; % Specify the uncertainty in observations
+
+uncertainty_adjustment = 1; % Adjuster for model uncertainty
+
+std_adjustment_parameters =1; %2% Variance adjuster for parameters
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+% Image handling parameters
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+Estimation_Type = ['GF',int2str(filter_simulation),'_PS',int2str(SimulationSettings.slope_time)]; % Estimation Type is an indication of what estimation is being performed for, here Gauss indicates that all staes are initialised as realisations from a Gaussian distribution
+                           % Also used for the saving of figures % GF Gauss
+                           % filter, PS parameter slope
+                           
+fig_save =1; % Save figures as .fig for future use
+
+printpdf =1; % Specify whether results should be printed to pdf
+
+Image_handling_model_output=[1;0];
+
+plot_uncertainty =1; % Plot covariance of all states
+
+Image_handling_states = [0 0 0 0 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 0 0 0 0 0]; % Here a decision is made whether to plot specific states, 
+                                                                            % if the value is one the relevant figure is plotted, otherwise it is not.
+                                                                            % The columns indicate the state to plot and the rows indicate whether the whole simulation or a zoomed in ploted should be plotted.
+                                                                            % Column one corresponds with state 1 and so forth.
+Image_handling_inputs = [0 0 0 0;0 0 0 0];  % Here a decision is made whether to plot specific states, 
+                                                                            % if the value is one the relevant figure is plotted, otherwise it is not.
+                                                                            % The columns indicate the state to plot and the rows indicate whether the whole simulation or a zoomed in ploted should be plotted.
+                                                                            % Here column 1-4 are Vp,Ve,Vsi and Vfi respectively.
+
+Image_handling_firing_rates = [0 0 0 0]; % Here a decision is made whether to plot specific states, 
+                                                                            % if the value is one the relevant figure is plotted, otherwise it is not.
+                                                                            % The columns indicate the firing rate to plot. this is a three image plot where the input potential population firing rate and output potential are plotted.
+                                                                            % Here column 1-4 are Vp,Ve,Vsi and Vfi respectively.
+
+Image_handling_multi = [1 0 1;0 0 0];%  % Here a decision is made whether to plot specific states, 
+                                                                            % if the value is one the relevant figure is plotted, otherwise it is not.
+                                                                            % The columns indicate the figures to be plotted.
+                                                                            % Here column 1-3 are for all the model states, all the model states inputs, all the model parameters including the input mean.
+Estimation_Error = 1; % Plt the error on each estimate on a single plot
+
+plot_uncertaintyMulti =0;
+% Zoom parameters (seconds)
+% ~~~~~~~~~~~~~~~~~
+
+tstart =0; % Starting time for zoom
+
+zoomtime = 10; % Duration of zoom
